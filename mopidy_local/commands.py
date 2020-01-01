376811c88689c7445849d96c2851363a9c94695d
Stein Magnus Jodal <stein.magnus@jodal.no>
2020-01-02 00:04:56 +0100

    scan: Split run() into four pieces

--- mopidy_local/commands.py
+++ mopidy_local/commands.py
@@ -62,19 +62,48 @@ class ScanCommand(commands.Command):
 
     def run(self, args, config):
         media_dir = pathlib.Path(config["local"]["media_dir"]).resolve()
-        scan_timeout = config["local"]["scan_timeout"]
-        flush_threshold = config["local"]["scan_flush_threshold"]
-        excluded_file_extensions = config["local"]["excluded_file_extensions"]
-        excluded_file_extensions = tuple(
-            file_ext.lower() for file_ext in excluded_file_extensions
-        )
-
         library = storage.LocalStorageProvider(config)
 
-        logger.info(f"Finding files in {media_dir.as_uri()} ...")
-        file_mtimes, file_errors = mtimes.find_mtimes(
-            media_dir, follow=config["local"]["scan_follow_symlinks"]
+        file_mtimes = self._find_files(
+            media_dir=media_dir,
+            follow_symlinks=config["local"]["scan_follow_symlinks"],
         )
+
+        uris_to_update, uris_in_library = self._check_tracks_in_library(
+            media_dir=media_dir,
+            file_mtimes=file_mtimes,
+            library=library,
+            force_rescan=args.force,
+        )
+
+        uris_to_update.update(
+            self._find_files_to_scan(
+                media_dir=media_dir,
+                file_mtimes=file_mtimes,
+                uris_in_library=uris_in_library,
+                excluded_file_exts=[
+                    file_ext.lower()
+                    for file_ext in config["local"]["excluded_file_extensions"]
+                ],
+            )
+        )
+
+        self._scan_metadata(
+            media_dir=media_dir,
+            file_mtimes=file_mtimes,
+            uris=uris_to_update,
+            library=library,
+            timeout=config["local"]["scan_timeout"],
+            flush_threshold=config["local"]["scan_flush_threshold"],
+            limit=args.limit,
+        )
+
+        library.close()
+        return 0
+
+    def _find_files(self, *, media_dir, follow_symlinks):
+        logger.info(f"Finding files in {media_dir.as_uri()} ...")
+        file_mtimes, file_errors = mtimes.find_mtimes(media_dir, follow=follow_symlinks)
         logger.info(f"Found {len(file_mtimes)} files in {media_dir.as_uri()}")
 
         if file_errors:
@@ -85,6 +114,11 @@ class ScanCommand(commands.Command):
         for name in file_errors:
             logger.debug(f"Scan error {file_errors[name]!r} for {name!r}")
 
+        return file_mtimes
+
+    def _check_tracks_in_library(
+        self, *, media_dir, file_mtimes, library, force_rescan
+    ):
         num_tracks = library.load()
         logger.info(f"Checking {num_tracks} tracks from library")
 
@@ -98,7 +132,7 @@ class ScanCommand(commands.Command):
             if mtime is None:
                 logger.debug(f"Removing {track.uri}: File not found")
                 uris_to_remove.add(track.uri)
-            elif mtime > track.last_modified or args.force:
+            elif mtime > track.last_modified or force_rescan:
                 uris_to_update.add(track.uri)
             uris_in_library.add(track.uri)
 
@@ -106,27 +140,39 @@ class ScanCommand(commands.Command):
         for local_uri in uris_to_remove:
             library.remove(local_uri)
 
+        return uris_to_update, uris_in_library
+
+    def _find_files_to_scan(
+        self, *, media_dir, file_mtimes, uris_in_library, excluded_file_exts
+    ):
+        uris_to_update = set()
+
         for absolute_path in file_mtimes:
             relative_path = absolute_path.relative_to(media_dir)
             local_uri = translator.path_to_local_track_uri(relative_path)
 
             if any(p.startswith(".") for p in relative_path.parts):
                 logger.debug(f"Skipped {local_uri}: Hidden directory/file")
-            elif relative_path.suffix.lower() in excluded_file_extensions:
+            elif relative_path.suffix.lower() in excluded_file_exts:
                 logger.debug(f"Skipped {local_uri}: File extension excluded")
             elif local_uri not in uris_in_library:
                 uris_to_update.add(local_uri)
 
         logger.info(f"Found {len(uris_to_update)} tracks which need to be updated")
+        return uris_to_update
+
+    def _scan_metadata(
+        self, *, media_dir, file_mtimes, uris, library, timeout, flush_threshold, limit
+    ):
         logger.info("Scanning...")
 
-        uris_to_update = sorted(uris_to_update, key=lambda v: v.lower())
-        uris_to_update = uris_to_update[: args.limit]
+        uris = sorted(uris, key=lambda v: v.lower())
+        uris = uris[:limit]
 
-        scanner = scan.Scanner(scan_timeout)
-        progress = _Progress(flush_threshold, len(uris_to_update))
+        scanner = scan.Scanner(timeout)
+        progress = _ScanProgress(batch_size=flush_threshold, total=len(uris))
 
-        for local_uri in uris_to_update:
+        for local_uri in uris:
             try:
                 relative_path = translator.local_uri_to_path(local_uri, media_dir)
                 file_uri = translator.path_to_file_uri(media_dir / relative_path)
@@ -153,13 +199,11 @@ class ScanCommand(commands.Command):
                     logger.debug("Progress flushed")
 
         progress.log()
-        library.close()
         logger.info("Done scanning")
-        return 0
 
 
-class _Progress:
-    def __init__(self, batch_size, total):
+class _ScanProgress:
+    def __init__(self, *, batch_size, total):
         self.count = 0
         self.batch_size = batch_size
         self.total = total
