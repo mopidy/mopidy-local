4cf798f1573ad33022c6b432e807ece24dc5f38d
Stein Magnus Jodal <stein.magnus@jodal.no>
2020-01-02 23:53:29 +0100

    translator: Add type hints

--- mopidy_local/translator.py
+++ mopidy_local/translator.py
@@ -1,18 +1,21 @@
+from __future__ import annotations
+
 import logging
 import os
-import pathlib
 import urllib
+from pathlib import Path
+from typing import Union
 
 logger = logging.getLogger(__name__)
 
 
-def local_uri_to_file_uri(local_uri, media_dir):
+def local_uri_to_file_uri(local_uri: str, media_dir: Path) -> str:
     """Convert local track or directory URI to file URI."""
     path = local_uri_to_path(local_uri, media_dir)
     return path.as_uri()
 
 
-def local_uri_to_path(local_uri, media_dir):
+def local_uri_to_path(local_uri: str, media_dir: Path) -> Path:
     """Convert local track or directory URI to absolute path."""
     if not local_uri.startswith("local:directory:") and not local_uri.startswith(
         "local:track:"
@@ -20,16 +23,17 @@ def local_uri_to_path(local_uri, media_dir):
         raise ValueError("Invalid URI.")
     uri_path = urllib.parse.urlsplit(local_uri.split(":", 2)[2]).path
     file_bytes = urllib.parse.unquote_to_bytes(uri_path)
-    file_path = pathlib.Path(os.fsdecode(file_bytes))
+    file_path = Path(os.fsdecode(file_bytes))
     return media_dir / file_path
 
 
-def path_to_file_uri(path):
+def path_to_file_uri(path: Union[str, bytes, Path]) -> str:
     """Convert absolute path to file URI."""
-    return pathlib.Path(os.fsdecode(path)).as_uri()
+    return Path(os.fsdecode(path)).as_uri()
 
 
-def path_to_local_track_uri(relpath):
+def path_to_local_track_uri(relpath: Union[str, bytes, Path]) -> str:
     """Convert path relative to :confval:`local/media_dir` to local track
     URI."""
-    return "local:track:%s" % urllib.parse.quote(os.fsencode(relpath))
+    bytes_path = os.fsencode(relpath)
+    return "local:track:%s" % urllib.parse.quote(bytes_path)
