019468edfa4a551fd2bbe43b961f52f43f32dd6f
Nick Steel <nick@nsteel.co.uk>
2021-02-24 14:29:47 +0000

    More liberal jpeg detection (Fixes #58) (#59)
    * More liberal jpeg detection (Fixes #58)
    
    imghdr does not detect all jpeg files, let's accept any data
    starting with the JPEG SOI marker.
    See https://stackoverflow.com/questions/36870661/imghdr-python-cant-detec-type-of-some-images-image-extension
    for more details.

--- mopidy_local/storage.py
+++ mopidy_local/storage.py
@@ -58,6 +58,15 @@ def get_image_size_jpeg(data):
     return width, height
 
 
+def test_jpeg(data, file_handle):
+    # Additional JPEG detection looking for JPEG SOI marker
+    if data[:2] == b"\xff\xd8":
+        return "jpeg"
+
+
+imghdr.tests.append(test_jpeg)
+
+
 class LocalStorageProvider:
     def __init__(self, config):
         self._config = ext_config = config[Extension.ext_name]
